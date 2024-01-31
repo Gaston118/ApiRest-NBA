c9b024a6c2cf5a130d158cc94784ee53e82b6160
Gaston Capdevila <gaston.capdevila118@gmail.com>
2024-01-31 01:57:53 -0300

    mostras equipos en cards

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { getAll } from './services/players';
+import TeamLogo from './components/TeamLogo';
 
-function App() {
-  const [count, setCount] = useState(0)
+const teams = [
+  'Atlanta Hawks',
+  'Boston Celtics',
+  'Brooklyn Nets',
+  'Charlotte Hornets',
+  'Chicago Bulls',
+  'Cleveland Cavaliers',
+  'Dallas Mavericks',
+  'Denver Nuggets',
+  'Detroit Pistons',
+  'Golden State Warriors',
+  'Houston Rockets',
+  'Indiana Pacers',
+  'LA Clippers',
+  'Los Angeles Lakers',
+  'Memphis Grizzlies',
+  'Miami Heat',
+  'Milwaukee Bucks',
+  'Minnesota Timberwolves',
+  'New Orleans Pelicans',
+  'New York Knicks',
+  'Oklahoma City Thunder',
+  'Orlando Magic',
+  'Philadelphia 76ers',
+  'Phoenix Suns',
+  'Portland Trail Blazers',
+  'Sacramento Kings',
+  'San Antonio Spurs',
+  'Toronto Raptors',
+  'Utah Jazz',
+  'Washington Wizards'
+]
+
+const App = () => {
+
+  const handleLogoClick = (teamName) => {
+    console.log(`Clic en el logo de ${teamName}`);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <img className="logo-nba" src="/assets/nba.svg" alt="NBA Logo" />
+      <h1 className='titulo'>NBA API</h1>
 
-export default App
+      <div className="team-grid">
+        {teams.map((team) => (
+          <div key={team} className="card">
+            <TeamLogo teamName={team} onClick={handleLogoClick} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ FRONTEND/src/components/TeamLogo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TeamLogo = ({ teamName, onClick }) => {
+  const imageName = teamName.replace(/\s/g, '_') + '.png';
+  const imagePath = `/assets/images/${imageName}`
+
+  return (
+    <div>
+    <img src={imagePath} alt={teamName} onClick={() => onClick(teamName)}/>
+    </div>
+  );
+};
+
+export default TeamLogo;
