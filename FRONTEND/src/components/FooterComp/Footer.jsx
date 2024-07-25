ec5279cba40bffda8364a8a71ec5485320aa4e12
Nicolas <nicolasseia@gmail.com>
2024-07-25 16:13:54 -0300

    update to merge

--- FRONTEND/src/components/FooterComp/Footer.jsx
+++ FRONTEND/src/components/FooterComp/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="flex items-center justify-center p-2 bg-gray-800 to-transparent h-12 w-70 z-10">
         <p className="flex font-bold text-white">
-            <a href="https://github.com/Gaston118" target="_blank">Gaston Capdevila</a> - <a href="https://github.com/NicoPrograms" target="_blank">Nicolas Seia</a>
+            <a href="https://github.com/Gaston118" target="_blank">Gaston Capdevila</a>  -  <a href="https://github.com/NicoPrograms" target="_blank">Nicolas Seia</a>
         </p>
     </div>
   )
